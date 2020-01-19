cba64f4861382e55f2ba481c4c9481b78ef1f47a
David Hepworth <davidhepp2@gmail.com>
2020-01-19 22:29:56 +0000

    added shell as sorta like a uri scheme in prompt

--- .bashrc.d/000.prompt.sh
+++ .bashrc.d/000.prompt.sh
@@ -3,19 +3,30 @@ FmtReset='\[$(tput sgr0)\]'
 
 FgWhite='\[\e[38;5;255m\]'
 FgRed='\[\e[38;5;197m\]'
-FgGreen='\[\e[38;5;40m\]'
 FgBlue='\[\e[38;5;45m\]'
+FgGreen='\[\e[38;5;40m\]'
 FgPurple='\[\e[38;5;5m\]'
+FgYellow='\[\e[38;5;226m\]'
 
 PROMPT_COMMAND=__prompt_command
 
 __prompt_command()
 {
   local ExitStatus="$?"
+  local ShellLevel="$SHLVL"
   PS1=""
 
   # Exit status
   [[ $ExitStatus != 0 ]] && PS1+="${FgRed}${ExitStatus} "
+
+  # shell (scheme?)
+  PS1+="${FgYellow}\s"
+
+  ## shell level
+  [[ $ShellLevel > 1 ]] && PS1+="[${ShellLevel}]"
+
+  ## scheme-authority separator
+  PS1+="${FgWhite}://"
 
   # user
   PS1+="${FgGreen}\u"
@@ -23,8 +34,8 @@ __prompt_command()
   # @host
   PS1+="${FgWhite}@${FgPurple}\h"
 
-  # :directory
-  PS1+="${FgWhite}:${FgBlue}\W"
+  # /directory
+  PS1+="${FgWhite}/${FgBlue}\W"
 
   # $ end
   PS1+="${FgRed} \$${FmtReset} "
